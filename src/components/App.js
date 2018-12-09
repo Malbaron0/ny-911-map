5eee57ef6e919e2ba86ebd376870c8ac6a164da2
Malyar Baron <malbaron0@gmail.com>
2018-12-09 15:08:52 -0500

    create states

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import LeafletMap from './LeafletMap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      nycOpenData: [],
+      error: ""
+    }
+
+  }
   render() {
     return (
       <LeafletMap/>
